544feeb6b12960545a7e3e343d96635f62bba16c
Mohammed Hizan P M <mhizan001@gmail.com>
2026-02-25 20:12:25 +0530

    Create ConvertTheTemp.py

---
+++ Day3/ConvertTheTemp.py
@@ -0,0 +1,8 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        kelvin = round(celsius + 273.15,5)
+        fahrenheit = round(celsius*1.80+32.00,5)
+        ans = []
+        ans.append(kelvin)
+        ans.append(fahrenheit)
+        return ans
